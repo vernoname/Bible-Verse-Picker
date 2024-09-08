d79b6f35a37e22986376688f70f2f5d2a4bbf734
Vernon AME <vernoname@icloud.com>
2024-09-08 10:37:39 -0500

    Initial Commit

---
+++ Bible Verse Picker/Bible_Verse_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bible_Verse_PickerApp.swift
+//  Bible Verse Picker
+//
+//  Created by Vernon AME on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Bible_Verse_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bible Verse Picker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bible Verse Picker
+//
+//  Created by Vernon AME on 9/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
